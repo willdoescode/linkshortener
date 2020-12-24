90d82039fefc8a686421457aa38c2c0340cb889b
Will Lane <williamlane923@gmail.com>
2020-12-23 17:12:50 -0800

    removed uneeded comments in server

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -27,8 +27,8 @@ var (
 	ctx           = context.Background()
 	rdb           = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Password: "",
+		DB:       0,
 	})
 )
 
